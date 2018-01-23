1f107cffdd2c565b5c6b78d8a3694425b0c4acf1
Tierra Smith <tsmith@wlapcs.org>
2018-01-23 19:08:26 +0000

    first commit

--- sketch.js
+++ sketch.js
@@ -1,8 +1,25 @@
+var rickyaSlider;
+var tierraSlider;
+var loveSlider;
 function setup(){
     createCanvas(600,400);
-    background(0);
+    background(244, 66, 155);
+
+   //createSiler(min, max, default)
+    rickyaSlider = createSlider(0,255,0);
+    //.position(x,y)
+     rickyaSlider.position(20,20);
+     tierraSlider = createSlider (0,255,0);
+     tierraSlider.position(20,50);
+     
+     loveSlider = createSlider(0,255,0);
+     loveSlider.position(20,80);
 }
 
 function draw(){
+    redValue = rickyaSlider.value();
+    tValue = tierraSlider.value();
+    lValue = loveSlider.value();
     
+    background(redValue,tValue ,lValue);
 }
